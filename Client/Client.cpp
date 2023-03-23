f7397a1cab258b51e730475c5e83b6ca8f206f29
Andrew Stanley <astanley6786@conestogac.on.ca>
2023-03-23 17:20:56 -0400

    PktDef created

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,1 +1,36 @@
+// Create client connection
+#include <windows.networking.sockets.h>
+#pragma comment(lib, "Ws2_32.lib")
+
 #include <iostream>
+#include <string>
+#include <fstream>
+#include "PktDef.h"
+#define MAX_NUM_OF_BYTES 1019
+
+using namespace std;
+
+//starts Winsock DLLs
+WSADATA wsaData;
+if ((WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0) {
+	return 0;
+}
+
+//initializes socket. SOCK_STREAM: TCP
+SOCKET ClientSocket;
+ClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+if (ClientSocket == INVALID_SOCKET) {
+	WSACleanup();
+	return 0;
+}
+
+//Connect socket to specified server
+sockaddr_in SvrAddr;
+SvrAddr.sin_family = AF_INET;						//Address family type itnernet
+SvrAddr.sin_port = htons(27000);					//port (host to network conversion)
+SvrAddr.sin_addr.s_addr = inet_addr("127.0.0.1");	//IP address
+if ((connect(ClientSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr))) == SOCKET_ERROR) {
+	closesocket(ClientSocket);
+	WSACleanup();
+	return 0;
+}
