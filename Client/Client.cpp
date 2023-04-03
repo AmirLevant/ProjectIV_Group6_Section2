c38fe4ceef26a3e86880e8597a0c1f8c87b95397
Amir Mansor <Amansor1893@conestogac.on.ca>
2023-04-03 10:42:24 -0400

    Server.cpp updated with initServer, connectToServer and runServer

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -46,6 +46,5 @@ void RunClient()
 {
 	SOCKET ClientSocket = InitClient();
 	ConnectToServer(ClientSocket);
-
-	
 }
+
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,11 +1,60 @@
-
-
 #include <iostream>
+#include <windows.networking.sockets.h>
+#include <string>
+#include <fstream>
+#include "PktDef.h"
 using namespace std;
 
-int main()
+
+int initServer()
 {
-    cout << "Hello World!\n";
+	//starts Winsock DLLs		
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	//create server socket
+	SOCKET ServerSocket;
+	ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (ServerSocket == INVALID_SOCKET) {
+		WSACleanup();
+		return 0;
+	}
+
+	return ServerSocket;
 }
+
+void connectToServer(SOCKET ServerSocket)
+{
+
+	//binds socket to address
+	sockaddr_in SvrAddr;
+	SvrAddr.sin_family = AF_INET;
+	SvrAddr.sin_addr.s_addr = INADDR_ANY;
+	SvrAddr.sin_port = htons(27000);
+	if (bind(ServerSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr)) == SOCKET_ERROR)
+	{
+		closesocket(ServerSocket);
+		WSACleanup();
+		return;
+	}
+
+	//listen on a socket
+	if (listen(ServerSocket, 1) == SOCKET_ERROR) {
+		closesocket(ServerSocket);
+		WSACleanup();
+		return;
+	}
+}
+
+void runServer()
+{
+	SOCKET ServerSocket = initServer();
+	connectToServer(ServerSocket);
+}
+
+
+
+
 
 
